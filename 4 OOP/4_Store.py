37e4ccb6cc955f909c7bae2e779d849035848f09
Brian Peitz <BPeitz08@gmail.com>
2018-01-15 21:02:26 -0800

    Add hospital

--- 4 OOP/4_Store.py
+++ 4 OOP/4_Store.py
@@ -1,50 +1,19 @@
-class product(object):
-    def __init__(self, price,item_name,weight,brand,status):
-        self.Price = price
-        self.Item_Name =  item_name
-        self.Weight = weight
-        self.Brand = brand
-        self.Status = "for sale"
-    def Display_All(self):
-        print "Price: ", '${:,.2f}'.format(self.Price)
-        print "Item Name: ", self.Item_Name
-        print "Weight(lbs): ", self.Weight
-        print "Brand: ", self.Brand
-        print "Status: ", self.Status
-        return self
-    def Sell(self):
-        self.Status = "sold"
-        self.Display_All()
-        return self
-    def AddTaxinDecimal(self,tax):
-        return (1 + tax) * self.Price
-    def Return(self, reason):
-        if reason == "defective":
-            self.Status = reason
-        elif reason == "in box":
-            self.Status = "for sale"
-        elif reason == "opened box":
-            self.Status = "used"
-            self.Price = 0.8 * self.Price
-        else:
-            self.Status = "for sale"
-        self.Display_All()
-
-class Store(object):
-    def __init__(self, products, location, owner):
-        self.Products = [products]
-        self.Location = location
-        self.Owner = owner
-        self.display_all()
-    def display_all(self):
-        print 'Products: ' + str(self.Products)
-        print 'Location: ' + str(self.Location)
-        print 'Owner: ' + self.Owner
-    def Add_Products(self,thisProduct):
-        self.Products.append(thisProduct)
-        self.display_all()
-    def Remove_Product(self,thisProduct):
-        self.Products.remove(thisProduct)
-        self.display_all()
-
-        
+from human import Human
+class Wizard(Human):
+    def __init__(self):
+        super(Wizard, self).__init__()   # use super to call the Human __init__ method
+        self.intelligence = 10           # every wizard starts off with 10 intelligence
+    def heal(self):
+        self.health += 10
+class Ninja(Human):
+    def __init__(self):
+        super(Ninja, self).__init__()    # use super to call the Human __init__ method
+        self.stealth = 10                # every Ninja starts off with 10 stealth
+    def steal(self):
+        self.stealth += 5
+class Samurai(Human):
+    def __init__(self):
+        super(Samurai, self).__init__()  # use super to call the Human __init__ method
+        self.strength = 10               # every Samurai starts off with 10 strength
+    def sacrifice(self):
+        self.health -= 5
--- 4 OOP/9_hospital.py
+++ 4 OOP/9_hospital.py
@@ -1,78 +1,68 @@
-
+#Assignment: Hospital
+#You're going to build a hospital with patients in it! Create a hospital class.
+#Before looking at the requirements below, think about the potential characteristics of each patient and hospital. How would you design each?
+#Patient:
+#Attributes:
+#- Id: an id number
+#- Name
+#- Allergies
+#- Bed number: should be none by default
 class Patient(object):
-    def __init__(self,ID,name,allergies,bed_number="none"):
-        self.ID = ID
-        self.name = name
+    def __init__(self,id,name,allergies,bed_number):
+        self.id = id
+        self.name=name
         self.allergies = allergies
         self.bed_number = bed_number
+        self.admitted = False
+#Hospital
+#Attributes:
+#- Patients: an empty array
+#- Name: hospital name
+#- Capacity: an integer indicating the maximum number of patients the hospital can hold.
 
-    def display_info(self):
-        print self.ID
-        print self.name
-        print self.allergies
-        print self.bed_number
-
-
-patient1 = Patient(1,"John Dow","dirt")
-patient2 = Patient(2,"Stan Lee","flower")
-patient3 = Patient(3,"Mary Jane","fish")
-patient4 = Patient(4,"Rob Roy","nut")
-
-patients= ["patient"+str(i) for i in range(1,5)]
-
-
-
-# for patient in patients:
-# 	patient.strip("\"").display_info()
-# patient1.display_info()
-
-
-
-
-
-
-class Bed(object):
-	def __init__(self,ID,bed_number,status="vacant"):
-		self.ID = ID
-		self.bed_number = bed_number
-		self.status = status
-
-	# def change_status(self):
-	# 	if
-
+#Methods:
+#- Admit: add a patient to the list of patients. Assign the patient a bed number. 
+# If the length of the list is >= the capacity do not admit the patient. 
+# Return a message either confirming that admission is complete or saying the hospital is full.
+#
+#- Discharge: look up and remove a patient from the list of patients. 
+# Change bed number for that patient back to none.
+#
+# This is a challenging assignment. Ask yourself what input each method requires and what output you will need.
 
 class Hospital(object):
-	def __init__(self,name,capacity):
-		self.name=name
-		self.capacity=capacity
-		self.patients=[]
-		self.bed_list=[]
-		for i in range(1,capacity+1):
-			self.bed_list.append(Bed("",i,))
+    def __init__(self, name, capacity=10, *these_patients):
+        self.patients = []
+        self.name = name
+        self.capacity = capacity
+        for this_patient in these_patients:
+            self.Admit(this_patient)
 
-# hospital1 = Hospital("Dallas Memorial",300)
-# print hospital1.bed_list
+    def Admit(self,this_patient):
+        admitted_count=0
+        for patient in self.patients:
+            if patient.admitted:
+                admitted_count += 1        
+        if admitted_count < self.capacity:            
+            this_patient.admitted = True
+            self.patients.append(this_patient)
+            print this_patient.name, "admitted"
+        else:
+            print "Hospital full."
+            print "Sorry ", this_patient.name,", your in the streeet"                
+        return self
 
-# filter = [bed for bed in hospital1.bed_list if bed.status=="vacant"]
-# print len(filter)
-	def admit(self, patient):
-		if len([bed for bed in bed_list if bed.status=="vacant"]) < self.capacity:
-			#alocate first vacant bed in the bed_list
-			[bed for bed in bed_list if bed.status=="vacant"][0].ID=patient.ID
-			[bed for bed in bed_list if bed.status=="vacant"][0].status="vacant"
-			bed_number_list.status="occupied"
-			self.patients.append(patient)
-			print "Your admission has been confirmed!"
-		else:
-			print "The hospital is full"
-		
-
-	def remove(self,patient):
-		patients.remove(patient)
-		if bed.ID==patient.ID:
-			bed.status="vacant"
-# hospital1=Hospital("Dallas Hospital", 10)
-# print hospital1.patients
-# print patient1.bed_number
-# print hospital1.bed_number_list
-# hospital1.admit(patient1)
+            
+    def Discharge(self,patient_name):
+        for this_patient in self.patients:
+            if patient_name == this_patient.name:
+                this_patient.admitted = False
+                this_patient.bed_number = 0
+                print this_patient.name, "discharged"
+                break
+ 
+# prove Patient class workds.
+#newPatient = Patient(23,"Carl Bruise","Peanuts",1)
+HATCHET_MEDICAL = Hospital("Hatchet Medical",3 ,Patient(23,"Carl Bruise", "Peanuts",1), Patient(24, "Sally Lame", "Walnuts", 1), Patient(25, "Bob Broken", "Filburts", 3),Patient(26, "Larry Largesore", "Peanuts", 1),Patient(27, "Shirly Bruise", "Peanuts", 4))
+HATCHET_MEDICAL.Discharge("Carl Bruise")
+HATCHET_MEDICAL.Admit(Patient(28,"Rocky Life", "none",1))
